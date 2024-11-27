bc0da45405e2b8cd1979d597f3ae703fab91cf8f
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-11-27 18:01:38 -0400

    changes

--- frontend/iot-frontend/src/components/parking-spot/parkingSpot.js
+++ frontend/iot-frontend/src/components/parking-spot/parkingSpot.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const ParkingSpot = () => {
+   const GEO_API_KEY = process.env.REACT_APP_GEO_API_KEY;
    const [info, setInfo] = useState(null);
    const [location, setLocation] = useState(null);
    const [error, setError] = useState(null);
@@ -22,7 +23,24 @@ const ParkingSpot = () => {
           setError("Geolocation is not supported by this browser.");
       }
   };
+
+  const fetchLocation = async () => {
+   const url = `https://api.opencagedata.com/geocode/v1/json?q=${location.latitude}+${location.longitude}&key=${GEO_API_KEY}`;
+
+   try {
+     const response = await axios.get(url);
+     if (response.data.results && response.data.results.length > 0) {
+      console.log(response.data.results[0].formatted);
+           } else {
+       setError("No location data found.");
+     }
+   } catch (err) {
+     setError("Failed to fetch location data.");
+   }
+ };
+
    useEffect(() => {
+      getLocation();
       setInfo(JSON.parse(localStorage.getItem("info")));
    }, []);
    
@@ -37,7 +55,7 @@ const ParkingSpot = () => {
                    <p>{`Tu email es: ${info.email}`}</p>
                    <p>{`Tu ubicación es: ${location ? `Latitud: ${location.latitude}, Longitud: ${location.longitude}` : "Cargando ubicación..."}`}</p>
                    {error && <p>{error}</p>}
-                   <button onClick={getLocation}>Get Location</button>
+                   <button onClick={fetchLocation}>Get Location</button>
                </div>
            )}
        </div>
