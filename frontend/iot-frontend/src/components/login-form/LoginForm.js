3c9fe5c03411b9152874cf938c3cfeb2beb8c160
Laaauuura <lauravictoriamallea@gmail.com>
2024-12-08 21:32:27 -0400

    Mejora estilos

--- frontend/iot-frontend/src/components/login-form/LoginForm.js
+++ frontend/iot-frontend/src/components/login-form/LoginForm.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
-import { Link } from 'react-router-dom'; // Asegúrate de usar React Router
-import './LoginForm.css';
+import { Link } from 'react-router-dom'; 
 import { ProgressBar } from 'primereact/progressbar';
+import Navbar from "../navbar/Navbar";
 import axios from 'axios';
 import { Message } from 'primereact/message';
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import './LoginForm.css';
+
 const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-
     const [isLoading, setIsLoading] = useState(false);
     const [response, setResponse] = useState(null);
     const navigate = useNavigate();
@@ -26,63 +27,63 @@ const LoginForm = () => {
     }, [response]);
 
     const handleLogin = (e) => {
-
-        
         e.preventDefault();
-        const credentials = {
-            username,
-            password
-        };
         setIsLoading(true);
-        axios.post(`${process.env.REACT_APP_API_URL}/users/auth`, credentials)
-            .then((response) => {
-                console.log(response.data);
+        axios.post(`${process.env.REACT_APP_API_URL}/users/auth`, { username, password })
+            .then((res) => {
                 setIsLoading(false);
-                setResponse(response.data);
+                setResponse(res.data);
             })
-            
+            .catch(() => {
+                setIsLoading(false);
+                setResponse({ success: false, message: "Error al iniciar sesión." });
+            });
     };
 
     return (
-        <div className="login-form-container">
-            <h2>Iniciar Sesión</h2>
-            <form onSubmit={handleLogin} className="p-fluid">
-                <div className="field">
-                    <label htmlFor="username" className="block">Usuario</label>
-                    <InputText
-                        id="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        placeholder="Ingrese su usuario"
-                        required
-                        className="w-full"
-                    />
+        <div>
+            <Navbar />
+            <div className="login-form-container">
+                <h2>Iniciar Sesión</h2>
+                <form onSubmit={handleLogin} className="p-fluid">
+                    <div className="field">
+                        <label htmlFor="username" className="block">Usuario</label>
+                        <InputText
+                            id="username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            placeholder="Ingrese su usuario"
+                            required
+                            className="w-full"
+                        />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="password" className="block">Contraseña</label>
+                        <Password
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            feedback={false}
+                            toggleMask
+                            placeholder="Ingrese su contraseña"
+                            required
+                            className="w-full"
+                        />
+                        {response && (
+                            <Message
+                                severity={response.success ? "success" : "error"}
+                                text={response.message}
+                            />
+                        )}
+                    </div>
+                    <div className="mt-4">
+                        {isLoading && <ProgressBar mode="indeterminate" />}
+                        <Button label="Iniciar Sesión" type="submit" className="p-button-primary w-full" disabled={isLoading} />
+                    </div>
+                </form>
+                <div className="mt-4 text-center">
+                    <p>¿No estás registrado? <Link to="/signup" className="signup-link">Regístrate</Link></p>
                 </div>
-                <div className="field mt-4">
-                    <label htmlFor="password" className="block">Contraseña</label>
-                    <Password
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        feedback={false}
-                        toggleMask
-                        placeholder="Ingrese su contraseña"
-                        required
-                        className="w-full"
-                    />
-                    {response != null && !response.success && <Message severity="error" text={response.message} />}
-                    {response != null && response.success && <Message severity="success" text={response.message} />}
-
-                </div>
-                <div className="mt-4" style={{ position: 'relative' }}>
-                    {isLoading &&
-                        <ProgressBar mode="indeterminate" style={{ height: '6px' }}></ProgressBar>
-                    }
-                        <Button label="Iniciar Sesión" type="submit" className="p-button-primary w-full"  disabled={isLoading} />
-                </div>
-            </form>
-            <div className="mt-4 text-center">
-                <p>¿No estás registrado? <Link to="/signup" className="signup-link">Regístrate</Link></p>
             </div>
         </div>
     );
