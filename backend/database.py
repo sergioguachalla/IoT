943a71d128d8a35b607d852e7ef82e80e156db50
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-12-09 09:53:26 -0400

    add docker file and env

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://postgres:postgres@localhost:5433/iot"
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL") or f"postgresql://postgres:postgres@localhost:5433/iot"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
